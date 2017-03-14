802998e437b573ff42f45f7ed7a9b8c80e4ea3ba
pengfox <pengfox@DESKTOP-5BRRE5H>
2017-03-14 15:28:38 +0900

    실습과제 : 2. 디미베네

--- JavaPractice/src/org/dimigo/basic/Operator.java
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -21,10 +21,11 @@ public class Operator {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int m = 1700000, n = 3, j = 1500;
+		long m = 1700000, n = 3, j = 1500;
 		System.out.println("<< 디미베네 연간 인건비>>");
 		System.out.printf("월 평균 급여 : %,d원%n점포 내 직원 수 : %d명%n점포 수 : %,d개%n", m, n, j);
-		System.out.printf("연간 인건비 : %,d", m*12*n*j);
+		System.out.printf("연간 인건비 : %,d원", m*n*j*12);
 	}
 
 }
+
