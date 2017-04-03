b25110c818d7a5029c3f68e7dd71d41b3a91aad9
pengfox <pengfox@DESKTOP-5BRRE5H>
2017-04-03 09:51:14 +0900

    실습과제 : 7. My Favorites

--- JavaPractice/src/org/dimigo/oop/Question.java
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -20,19 +20,18 @@ import java.util.Scanner;
 public class Question {
 
 	public static void main(String[] args) {
-	String[] questions = {"가수는", "배우는", "과목은"};
-	String[] answers = {"아이유", "러셀 크로우", "응용 프로그래밍"};
+	StringBuilder tmp = new StringBuilder("<< 결과 출력 >>\n");
+	String[] questions = {"가수는", "배우는", "과목은"}, answers = {"아이유", "러셀 크로우", "응용 프로그래밍"};
 	
 	Scanner scanner  = new Scanner(System.in);
 	
 	for(int i = 0; i < 3; i ++) {
 		System.out.println((i + 1) + ". 가장 좋아하는 " + questions[i] + "?");
 		System.out.println(scanner.nextLine().equals(answers[i]) ? "정답입니다!" : "틀렸습니다!");
+		tmp.append("가장 좋아하는 " + questions[i] + " " + answers[i] + "입니다.\n");
 	}
 	scanner.close();
-	System.out.println("<< 결과 출력 >>");
-	for(int i = 0; i < 3; i ++)
-		System.out.println(new StringBuilder("가장 좋아하는 " + questions[i] + " " + answers[i] + "입니다."));
+	System.out.println(tmp);
 
 	}
 
