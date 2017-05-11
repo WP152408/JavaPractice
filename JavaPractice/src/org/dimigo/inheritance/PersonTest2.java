96ede8b06d02e7fd1c0ea342d4d1963f22e8bee3
pengfox <pengfox@DESKTOP-5BRRE5H>
2017-05-11 15:28:59 +0900

    실습과제 : 10. We Are the World!! fixed

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    l_ PersonTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 10.
+ * </pre>
+ * 
+ * @author : pengfox
+ * @version : 1.0
+ */
+public class PersonTest2 {
+	public static void main(String[] args) {
+		Person[] person = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+
+		for(Person p : person)
+			greeting(p);
+	}
+
+	private static void greeting(Person p) {
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+}
