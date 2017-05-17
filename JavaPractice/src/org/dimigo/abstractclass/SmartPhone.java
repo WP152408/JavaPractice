c0755de89b9a65f9e20309d26bf01713ff61f4eb
pengfox <pengfox@DESKTOP-5BRRE5H>
2017-05-17 10:46:25 +0900

    실습과제 : 12. 스마트폰 경쟁2

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    l_ SmartPhone
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ * 
+ * @author : pengfox
+ * @version : 1.0
+ */
+public abstract class SmartPhone {
+	private String model;
+	private String company;
+	private int price;
+	public SmartPhone() {
+		
+	}
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
+	}
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
+	}
+	public abstract void pay();
+	
+	public void useSpecialFunction() {
+		if (this instanceof IPhone) ((IPhone)this).useAirDrop();
+		if (this instanceof Galaxy) ((Galaxy)this).useWireless();
+	}
+	public String toString() {
+		return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : " +  String.format("%,d", price) + "원";
+	}
+}
---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *    l_ SmartPhoneTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ * 
+ * @author : pengfox
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone[] phones = {
+				new IPhone("iPhone 7", "애플", 900000),
+				new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		for(SmartPhone phone : phones) {
+			System.out.println(phone);
+			phone.turnOn();
+			phone.pay();
+			phone.useSpecialFunction();
+			phone.turnOff();
+			System.out.println();
+		}
+	}
+
+}
